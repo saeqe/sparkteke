a1672c5514e707420d4fc470f427f5dd5ac00e6d
ArmanIrbot <tgclibot@gmail.com>
2015-11-20 19:17:46 +0330

    Create arm.lua

---
+++ plugins/arm.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return " ArmAn Is KiNg OF bOtS 😘
+end
+return {
+  description = "Invite bot into a group chat", 
+  usage = "!join [invite link]",
+  patterns = {
+    "^/arman$",
+    "^arman$",
+    "^Arman$",
+    "^!arman$",
+  },
+  run = run
+}
+end
