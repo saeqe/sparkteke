d53dd836bfd14fa944c5bcb97625f4605c7ac666
ArmanIrbot <tgclibot@gmail.com>
2015-11-20 21:44:59 +0330

    Update grouphelp.lua

--- plugins/grouphelp.lua
+++ plugins/grouphelp.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-  return " For Kick User🔽\n!kick @username\n!kick @userid\nFor Ban User🔽\n!ban @username\n!ban userid\nFor Lock Group Name/Photo/Members🔽\n!group lock Name/Photo/Member\nFor Promoting User🔽\n!promote @username\nFor Demoting User🔽\n!demote @username\nFor Setting New Rules/Photo/Name🔽\n!setNewRules/Photo/Name\nFor Getting Your Id Or Group You Are in🔽\n!id\nFor Returning All Members Id🔽\n!ids chat\nFor Returning A User's Id🔽\n!id @username\nFor Enabling Ani-Bot🔽\n!antibot enable\nFor Disable Anti-Bot🔽\n!antibot disable\nFor Allow A Bot🔽\n!antibot allow Botid\nFor Disallow A Bot🔽\n!antibot disallow botid\nYou Can Set Limit Flood With🔽\n!limit maxfloodnumber\nBest Wishes🔽\n@ThisIsArman 
+  return " For Kick User🔽\n!kick @username\n!kick @userid\nFor Ban User🔽\n!ban @username\n!ban userid\nFor Lock Group Name/Photo/Members🔽\n!group lock Name/Photo/Member\nFor Promoting User🔽\n!promote @username\nFor Demoting User🔽\n!demote @username\nFor Setting New Rules/Photo/Name🔽\n!setNewRules/Photo/Name\nFor Getting Your Id Or Group You Are in🔽\n!id\nFor Returning All Members Id🔽\n!ids chat\nFor Returning A User's Id🔽\n!id @username\nFor Enabling Ani-Bot🔽\n!antibot enable\nFor Disable Anti-Bot🔽\n!antibot disable\nFor Allow A Bot🔽\n!antibot allow Botid\nFor Disallow A Bot🔽\n!antibot disallow botid\nYou Can Set Limit Flood With🔽\n!limit maxfloodnumber(ItWillBanSpammer)\n You Can Set Limit Flood With🔽\n!limit1 maxfloodnumber (ItWillKickSpammer)\nBest Wishes🔽\n@ThisIsArman" 
 return {
   description = "Invite bot into a group chat", 
   usage = "!join [invite link]",
