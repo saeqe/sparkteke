e6b57e7710786a185e0124a2420019469798435c
ArmanIrbot <tgclibot@gmail.com>
2015-11-20 21:58:00 +0330

    Update antiflood5.lua

--- plugins/antiflood5.lua
+++ plugins/antiflood5.lua
@@ -78,8 +78,8 @@ return {
   description = 'Plugin to kick flooders from group.',
   usage = {},
   patterns = {
-    '^!antiflood1 (enable)$',
-    '^!antiflood1 (disable)$'
+    '^!limit5 (enable)$',
+    '^!limit5 (disable)$'
   },
   run = run,
   privileged = true,
