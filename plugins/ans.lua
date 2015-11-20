01e2816f92dbc479c22e6de2448feb1b0b8e040c
ArmanIrbot <tgclibot@gmail.com>
2015-11-20 19:16:26 +0330

    Create ans.lua

---
+++ plugins/ans.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return " SUCK IT OK? "
+end
+return {
+  description = "Invite bot into a group chat", 
+  usage = "!join [invite link]",
+  patterns = {
+    "^/fuck$",
+    "^!fuck$",
+    "^/Fuck$",
+    "^!Fuck$",
+  },
+  run = run
+}
+end
