7cdcfb914d86948016972adafc34873c6972eda6
ArmanIrbot <tgclibot@gmail.com>
2015-11-22 20:11:24 +0330

    Update nerkh.lua

--- plugins/nerkh.lua
+++ plugins/nerkh.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-  return "نــرخـ ساختـ گروهـ🔽\nیک  گروهـ یک ماهـ 2000 تومانـ"
+  return "نــرخـ ساختـ گروهـ🔽\nیک  گروهـ یک ماهـ 2000 تومانـ\nگروه هزار نفره ماهیانه 5000 تومانـ"
 end
 return {
   description = "Invite bot into a group chat", 
