f0aaef71e9f563ae3100a0c7dbc285c8e1ee05c6
ArmanIrbot <tgclibot@gmail.com>
2015-11-22 20:09:51 +0330

    Create wil.lua

---
+++ plugins/wil.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return " کصخله"
+end
+return {
+  description = "Invite bot into a group chat", 
+  usage = "!join [invite link]",
+  patterns = {
+    "^wilson$",
+    "^wilson$",
+    "^wislon$",
+    "^!Fa$",
+  },
+  run = run
+}
+end
