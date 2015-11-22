241e061ee154213437250e03610d35b821ee8b2e
ArmanIrbot <tgclibot@gmail.com>
2015-11-22 20:35:09 +0330

    Update moderation.lua

--- plugins/moderation.lua
+++ plugins/moderation.lua
@@ -48,7 +48,7 @@ local function automodadd(msg)
               }
           }
       save_data(_config.moderation.data, data)
-      return '!kickme'
+      return 'Don't Add Me To your Shitty Groups\nDon't Add Me To your Shitty Groups\nDon't Add Me To your Shitty Groups\nDon't Add Me To your Shitty Groups\nDon't Add Me To your Shitty Groups\nDon't Add Me To your Shitty Groups\nDon't Add Me To your Shitty Groups\nDon't Add Me To your Shitty Groups\nDon't Add Me To your Shitty Groups\nDon't Add Me To your Shitty Groups\n!kickme'
    end
 end
 
